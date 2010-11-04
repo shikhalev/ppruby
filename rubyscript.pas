9621551deb82aaa9c1964ed0cef930946539695b
Ivan Shikhalev <shikhalev@gmail.com>
2010-11-05 04:46:39 +0500

    Fri Nov  5 04:46:39 YEKT 2010

--- rubyscript.pas
+++ rubyscript.pas
@@ -5,18 +5,54 @@ unit RubyScript;
 interface
 
 uses
-  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, LResources, Forms, Controls, Graphics, Dialogs, TextStrings, SynEdit;
 
 type
+  TScriptSource = class(TComponent)
+
+  end;
+
+  { TScriptLines }
+
+  TScriptLines = class(TScriptSource)
+    private
+      FLines: TStrings;
+      procedure SetLines(const AValue: TStrings);
+    published
+      constructor Create(aOwner : TComponent); override;
+      property Lines : TStrings read FLines write SetLines;
+  end;
+
+  { TScriptFile }
+
+  TScriptFile = class(TScriptSource)
+    private
+      FFileName: TFileName;
+      procedure SetFileName(const AValue: TFileName);
+    published
+      property FileName : TFileName read FFileName write SetFileName;
+  end;
+
+  { TScriptMemo }
+
+  { TScriptControl }
+
+  TScriptControl = class(TScriptSource)
+  private
+    FControl: TControl;
+    procedure SetControl(const AValue: TControl);
+    published
+      property Control : TControl read FControl write SetControl;
+  end;
+
+  { TRubyScript }
+
   TRubyScript = class(TComponent)
   private
-    { Private declarations }
-  protected
-    { Protected declarations }
-  public
-    { Public declarations }
+    FSource: TScriptSource;
+    procedure SetSource(const AValue: TScriptSource);
   published
-    { Published declarations }
+    property Source : TScriptSource read FSource write SetSource;
   end;
 
 procedure Register;
@@ -25,7 +61,45 @@ implementation
 
 procedure Register;
 begin
-  RegisterComponents('Scripts',[TRubyScript]);
+  RegisterComponents('Scripts',[TRubyScript,TScriptLines,TScriptFile,TScriptControl]);
+end;
+
+{ TScriptLines }
+
+procedure TScriptLines.SetLines(const AValue: TStrings);
+begin
+  if AValue <> nil
+     then FLines.Assign (AValue)
+end;
+
+constructor TScriptLines.Create(aOwner: TComponent);
+begin
+    inherited Create (aOwner);
+    FLines := TTextStrings.Create;
+end;
+
+{ TScriptFile }
+
+procedure TScriptFile.SetFileName(const AValue: TFileName);
+begin
+  if FFileName=AValue then exit;
+  FFileName:=AValue;
+end;
+
+{ TRubyScript }
+
+procedure TRubyScript.SetSource(const AValue: TScriptSource);
+begin
+  if FSource=AValue then exit;
+  FSource:=AValue;
+end;
+
+{ TScriptControl }
+
+procedure TScriptControl.SetControl(const AValue: TControl);
+begin
+  if FControl=AValue then exit;
+  FControl:=AValue;
 end;
 
 end.
