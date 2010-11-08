a159e5bd6ae1b4b3d324ae23c366b76eaf51b7ae
Ivan Shikhalev <shikhalev@gmail.com>
2010-11-08 06:42:05 +0500

    Clear

--- ppruby18.pp
+++ ppruby18.pp
@@ -14,20 +14,8 @@ unit ppRuby18;
 interface
 
 uses
-  Variants,
   Ruby18;
 
-function Value2Variant (value : VALUE) : variant;
-function Variant2Value (value : variant) : VALUE;
-
 implementation
-
-function Value2Variant (value : VALUE) : variant;
- begin
- end;
-
-function Variant2Value (value : variant) : VALUE;
- begin
- end;
 
 end.
