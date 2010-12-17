166a7875388a542a7059830eb855483e90a0a3b4
Ivan Shikhalev <shikhalev@gmail.com>
2010-12-17 22:28:20 +0500

    *

---
+++ ruby18lazlcl.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. Do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit Ruby18LazLCL; 
+
+interface
+
+uses
+  rb18Script, rb18LazLCLRegister, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+  RegisterUnit('rb18LazLCLRegister', @rb18LazLCLRegister.Register); 
+end; 
+
+initialization
+  RegisterPackage('Ruby18LazLCL', @Register); 
+end.
