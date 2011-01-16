16005c13ef765d0b7f50249b6ced737b5b0ad31a
Ivan Shikhalev <shikhalev@gmail.com>
2011-01-17 01:51:35 +0500

    + Demo

--- Demo/RubyDemo.lpr
+++ Demo/RubyDemo.lpr
@@ -7,7 +7,7 @@ uses
   cthreads,
   {$ENDIF}{$ENDIF}
   Interfaces, // this includes the LCL widgetset
-  Forms, MainForm;
+  Forms, MainForm, rubyfcl, rubylcl;
 
 {$R *.res}
 
--- Demo/mainform.pp
+++ Demo/mainform.pp
@@ -5,10 +5,23 @@ unit MainForm;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs; 
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  ExtCtrls, ppRuby, ppRubyForms;
 
 type
+
+  { TfrmMain }
+
   TfrmMain = class(TForm)
+    btnRun : TButton;
+    memAdd : TMemo;
+    memOut : TMemo;
+    memScript : TMemo;
+    pnlRight : TPanel;
+    pnlLeft : TPanel;
+    splTB : TSplitter;
+    splLR : TSplitter;
+    procedure btnRunClick(Sender : TObject);
   private
     { private declarations }
   public
@@ -21,6 +34,18 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.btnRunClick(Sender : TObject);
+ var
+   oldActive : Boolean;
+ begin
+  oldActive := ppRuby.Active;
+  ppRuby.Active := true;
+  EvalString(memScript.Lines.Text);
+  ppRuby.Active := oldActive;
+ end;
 
 end.
 
