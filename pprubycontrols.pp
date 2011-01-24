6f7f79d1a7ce3d2117b8e74a804617cce10180e3
Ivan Shikhalev <shikhalev@gmail.com>
2011-01-24 08:59:04 +0500

    * Mon Jan 24 08:59:04 YEKT 2011

--- pprubycontrols.pp
+++ pprubycontrols.pp
@@ -27,11 +27,47 @@ function m_tcontrol_refresh (instance : VALUE) : VALUE; cdecl;
   Result := instance;
  end;
 
+function m_tcontrol_parent (instance : VALUE) : VALUE; cdecl;
+ begin
+  Result := VALUE((TObject(instance) as TControl).Parent);
+ end;
+
+function m_tcontrol_parent_set (instance : VALUE; parent : VALUE) : VALUE; cdecl;
+ begin
+  (TObject(instance) as TControl).Parent := TObject(parent) as TWinControl;
+  Result := parent;
+ end;
+
 procedure TControlClassHook (cControl : VALUE);
  begin
   DefineMethod(cControl, 'hide', @m_tcontrol_hide);
   DefineMethod(cControl, 'show', @m_tcontrol_show);
   DefineMethod(cControl, 'refresh', @m_tcontrol_refresh);
+  DefineMethod(cControl, 'parent', @m_tcontrol_parent);
+  DefineMethod(cControl, 'parent=', @m_tcontrol_parent_set);
+ end;
+
+function m_twincontrol_canfocus (instance : VALUE) : VALUE; cdecl;
+ begin
+  Result := VALUE((TObject(instance) as TWinControl).CanFocus);
+ end;
+
+function m_twincontrol_focused (instance : VALUE) : VALUE; cdecl;
+ begin
+  Result := VALUE((TObject(instance) as TWinControl).Focused);
+ end;
+
+function m_twincontrol_setfocus (instance : VALUE) : VALUE; cdecl;
+ begin
+  (TObject(instance) as TWinControl).SetFocus;
+  Result := instance;
+ end;
+
+procedure TWinControlClassHook (cWinControl : VALUE);
+ begin
+  DefineMethod(cWinControl, 'canfocus?', @m_twincontrol_canfocus);
+  DefineMethod(cWinControl, 'focused?', @m_twincontrol_focused);
+  DefineMethod(cWinControl, 'setfocus', @m_twincontrol_setfocus);
  end;
 
 initialization
--- pprubyforms.pp
+++ pprubyforms.pp
@@ -7,22 +7,9 @@ interface
 uses
   Classes, SysUtils, ppRuby, ppRubyClasses, Forms;
 
-operator explicit (v : VALUE) : TWindowState;
-operator explicit (v : TWindowState) : VALUE;
-
 implementation
 
 // TODO: TCustomForm, TApplication... and all others
-
-var
-  cacheWindowStates : array [TWindowState] of VALUE;
-
-procedure InitWindowStates;
- begin
-  cacheWindowStates[wsNormal]    := VALUE(ID('wsNormal'));
-  cacheWindowStates[wsMinimized] := VALUE(ID('wsMinimized'));
-  cacheWindowStates[wsMaximized] := VALUE(ID('wsMaximized'));
- end;
 
 function gf_application : VALUE; cdecl;
  begin
@@ -40,25 +27,6 @@ procedure InitFunctions;
   DefineFunction('screen', @gf_screen);
  end;
 
-operator explicit (v : VALUE) : TWindowState;
- begin
-  case ValType(v) of
-       rtFixNum :
-         exit(TWindowState(PtrInt(v)));
-       rtSymbol :
-         for Result := Low(TWindowState) to High(TWindowState) do
-             if cacheWindowStates[Result] = v
-                then exit;
-  end;
-  raise ERubyConversion.CreateFmt(msgCanNotConvertTo, [ansistring(Inspect(v)), 'TWindowState']);
- end;
-
-operator explicit (v : TWindowState) : VALUE;
- begin
-  Result := cacheWindowStates[v];
- end;
-
 initialization
- ppRuby.AddLoadHook(@InitWindowStates);
  ppRuby.AddLoadHook(@InitFunctions);
 end.
