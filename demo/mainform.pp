c2ea240cd34882601b8a878045b4c4029cad61f7
Ivan Shikhalev <shikhalev@gmail.com>
2013-04-27 08:08:24 +0600

    Sat Apr 27 08:08:24 YEKT 2013

---
+++ demo/mainform.pp
@@ -0,0 +1,26 @@
+unit MainForm;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+
+type
+  TfrmMain = class(TForm)
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
