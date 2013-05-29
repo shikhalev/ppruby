baf4121d2eef274b3472f63c4be23584b60c1581
Ivan Shikhalev <shikhalev@gmail.com>
2013-05-29 19:17:19 +0600

    Wed May 29 19:17:19 YEKT 2013

--- demo/mainform.pp
+++ demo/mainform.pp
@@ -5,10 +5,16 @@ unit MainForm;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs;
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Ruby19Consts;
 
 type
+
+  { TfrmMain }
+
   TfrmMain = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { private declarations }
   public
@@ -21,6 +27,13 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TfrmMain }
+
+procedure TfrmMain.Button1Click(Sender: TObject);
+begin
+  Button1.Caption:= IntToStr(Integer(ruby_special_consts.RUBY_IMMEDIATE_MASK));
+end;
 
 end.
 
