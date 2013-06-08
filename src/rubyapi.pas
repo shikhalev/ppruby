4073a900a0132391abad71119b46b40bc59f7d2e
Ivan Shikhalev <shikhalev@gmail.com>
2013-06-09 05:43:53 +0600

    Sun Jun  9 05:43:53 YEKT 2013

--- src/rubyapi.pas
+++ src/rubyapi.pas
@@ -1,8 +1,5 @@
-{$if defined(RUBY20)}
-  {$define RUBY19}
-{$endif}
-
 {$mode ObjFPC}{$H+}
+{$packrecords C}
 
 unit RubyAPI;
 
@@ -43,15 +40,50 @@ const
 
 type
   VALUE = type PtrUInt;
+  PVALUE = ^VALUE;
   ID = type PtrUInt;
 
 // common functions
 
 procedure rb_alias (klass : VALUE; name, def : ID); cdecl; external RUBYLIB;
+procedure rb_alias_variable (name1, name2 : ID); cdecl; external RUBYLIB;
+function rb_any_to_s (obj : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_apply (recv : VALUE; mid : ID; args : VALUE) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_Array (val : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_aref (argc : cint; argv : PVALUE; ary : VALUE) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_ary_assoc (ary, key : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_clear (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_cmp (ary1, ary2 : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_concat (x, y : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_delete (ary, item : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_delete_at (ary : VALUE; pos : clong) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_ary_dup (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_each (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_entry (ary : VALUE; offset : clong) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_ary_freeze (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_includes (ary, item : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_join (ary, sep : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_new : VALUE; cdecl; external RUBYLIB;
+function rb_ary_new2 (len : clong) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_new3 (n : clong) : VALUE; cdecl; varargs; external RUBYLIB;
+function rb_ary_new4 (n : clong; elts : PVALUE) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_ary_plus (x, y : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_pop (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_push (ary, item : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_rassoc (ary, value : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_reverse (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
 
 // common variables
 
-{$if defined(RUBY19)}
+var
+  rb_argv0 : VALUE; cvar; external RUBYLIB;
+
+{$if defined(RUBY19) or defined(RUBY20)}
 
 // Ruby 1.9 types
 
@@ -64,6 +96,15 @@ type
 
 procedure rb_add_event_hook (func : rb_event_hook_func_t;
   events : rb_event_flag_t; data : VALUE); cdecl; external RUBYLIB;
+function rb_alloc_tmp_buffer (out store : VALUE; len : cint) : Pointer; cdecl;
+  external RUBYLIB;
+procedure rb_ary_free (ary : VALUE); cdecl; external RUBYLIB;
+function rb_ary_memsize (ary : VALUE) : csize_t; cdecl; external RUBYLIB;
+procedure rb_ary_modify (ary : VALUE); cdecl; external RUBYLIB;
+function rb_ary_replace (copy, orig : VALUE) : VALUE; cdecl; external RUBYLIB;
+function rb_ary_resize (ary : VALUE; len : clong) : VALUE; cdecl;
+  external RUBYLIB;
+function rb_ary_resurrect (ary : VALUE) : VALUE; cdecl; external RUBYLIB;
 
 // Ruby 1.9 variables
 
@@ -84,8 +125,13 @@ type
 
 procedure rb_add_event_hook (func : rb_event_hook_func_t; events : rb_event_t);
   cdecl; external RUBYLIB;
+procedure rb_add_method (klass : VALUE; mid : ID; node : PRNode; noex : cint);
+  cdecl; external RUBYLIB;
 
 // Ruby 1.8 variables
+
+var
+  rb_argv : VALUE; cvar; external RUBYLIB;
 
 {$endif}
 
